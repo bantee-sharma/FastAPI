41c1decf5c5424990af395cbc257bb4cb96f15b5
bantee-sharma <banteesharma9@gmail.com>
2025-05-13 21:26:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def hello():
+    return {"message" :"Hello world!"}
+
+@app.get('/predict')
+def func():
+    return {"message":"CampusX is a education platform where we can learn AI"}
