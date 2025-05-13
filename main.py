b911057b8168242718216545e4d07ba9e4a68bc9
bantee-sharma <banteesharma9@gmail.com>
2025-05-13 21:39:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 def hello():
     return {"message" :"Hello world!"}
 
+
 @app.get('/predict')
 def func():
     return {"message":"CampusX is a education platform where we can learn AI"}
