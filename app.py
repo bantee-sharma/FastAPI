d2c64c42aa8e3e08518518e0180b89cbf0c248bb
bantee-sharma <banteesharma9@gmail.com>
2025-05-15 17:29:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 app = FastAPI()
 
@@ -22,3 +22,12 @@ def view():
     data = load_data()
 
     return data
+
+
+@app.get('/search/{patient_id}')
+def search(patient_id: str = Path(..., title='Patient ID', description='ID of the patient', example='P001')):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    return {'Error':'Patient not found'}
