0c9682fe2738a7ea8ed9a15a54063ef021166817
bantee-sharma <banteesharma9@gmail.com>
2025-05-17 16:26:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def view():
 
 
 @app.get('/patient/{patient_id}')
-def search(patient_id: str = Path(..., description="ID of the patient in the DB", example='P001')):
+def search(patient_id: str = Path(..., description="ID of the patient in the DB",
+                                   example='P001')):
     data = load_data()
 
     if patient_id in data:
