08aa2cce05459b3c5b20e3edb65f6d2f2f623e47
bantee-sharma <banteesharma9@gmail.com>
2025-05-17 16:37:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def search(patient_id: str = Path(..., description="ID of the patient in the DB"
     raise HTTPException(status_code=404,detail="Patient not found")
 
 @app.get('/sort')
-def sort_patients(sort_by:str = Query(...,description="sort on the basis of Hieght,weighta and bmi"))
+def sort_patients(sort_by:str = Query(...,description="sort on the basis of Hieght,weighta and bmi", order:str = Query(...)))
