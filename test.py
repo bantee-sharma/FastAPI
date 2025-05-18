216265cd8e51f1dca24474eaefc396a71e71b69d
bantee-sharma <banteesharma9@gmail.com>
2025-05-18 16:39:24 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get('/')
+def hello():
+    return {"message":"Patients Management System API"}
+
+@app.get('/about')
+def about():
+    return {"message":'A fully functional API to manage your patient records'}
