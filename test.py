b9593b6fa4804425104e3ad11b9f9f7e5ba46a93
bantee-sharma <banteesharma9@gmail.com>
2025-05-18 20:57:34 +0530

    Update test.py

--- test.py
+++ test.py
@@ -22,12 +22,11 @@ def view():
     data = data_load()
     return data
 
-@app.get('/patient/{patient_id}')
-def patients(patient_id:str = Path(...,description="ID of patient in DB",example='P001')):
+@app.get("/patient/{patient_id}")
+def patients(patient_id:str=Path(...,description="ID of patient in DB",example='P001')):
     data = data_load()
-    
+
     if patient_id in data:
         return data[patient_id]
-    raise HTTPException(status_code=404,detail="No patinet found")
-    
+    raise HTTPException(status_code=404, detail="Patient not found")
 
