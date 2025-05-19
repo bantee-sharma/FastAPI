87650cfaf1203bf49f5d8d1436493a0db6161e45
bantee-sharma <banteesharma9@gmail.com>
2025-05-19 17:39:12 +0530

    Create n.py

---
+++ n.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI,HTTPException,Path
+import json
+
+def data_load():
+    with open("patients.json","rb") as f:
+        data = json.load(f)
+        return data
+
+app = FastAPI()
+
+@app.get('/')
+def hello():
+    return {'Message':"Patients Management System API"}
+
+@app.get('/about')
+def about():
+    return {"Message":"A fully functional API to manage your patient records"}
+
+@app.get('/view')
+def view():
+    data = data_load()
+    return data
+
+@app.get("/patient/{patient_id}")
+def patient(patient_id:str = Path()):
+    data = data_load()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise  HTTPException(status_code=404,detail="Patient Not found")
+
+
