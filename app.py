155f06900dd4b87f06b32d446350b292ca3b308c
bantee-sharma <banteesharma9@gmail.com>
2025-05-29 16:26:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,23 @@ from pydantic import BaseModel, Field, computed_field
 from typing import Literal,Annotated
 import pandas as pd
 import pickle
+from fastapi.responses import JSONResponse
+
 
 with open("model.pkl","rb") as f:
     model = pickle.load(f)
 
 app = FastAPI()
 
+tier_1_cities = ["Mumbai", "Delhi", "Bangalore", "Chennai", "Kolkata", "Hyderabad", "Pune"]
+tier_2_cities = [
+    "Jaipur", "Chandigarh", "Indore", "Lucknow", "Patna", "Ranchi", "Visakhapatnam", "Coimbatore",
+    "Bhopal", "Nagpur", "Vadodara", "Surat", "Rajkot", "Jodhpur", "Raipur", "Amritsar", "Varanasi",
+    "Agra", "Dehradun", "Mysore", "Jabalpur", "Guwahati", "Thiruvananthapuram", "Ludhiana", "Nashik",
+    "Allahabad", "Udaipur", "Aurangabad", "Hubli", "Belgaum", "Salem", "Vijayawada", "Tiruchirappalli",
+    "Bhavnagar", "Gwalior", "Dhanbad", "Bareilly", "Aligarh", "Gaya", "Kozhikode", "Warangal",
+    "Kolhapur", "Bilaspur", "Jalandhar", "Noida", "Guntur", "Asansol", "Siliguri"
+]
 
     # pydantic model to validate incoming data
 class UserInput(BaseModel):
@@ -48,3 +59,30 @@ class UserInput(BaseModel):
         elif self.age < 60:
             return "middle_aged"
         return "senior"
+    
+    @computed_field
+    @property
+    def city_tier(self) -> int:
+        if self.city in tier_1_cities:
+            return 1
+        elif self.city in tier_2_cities:
+            return 2
+        else:
+            return 3
+        
+
+
+@app.post("/predict")
+def predict_prmium(data: UserInput):
+    input_df = pd.DataFrame([{
+        'bmi': data.bmi,
+        'age_group': data.age_group,
+        'lifestyle_risk': data.lifestyle_risk,
+        'city_tier': data.city_tier,
+        'income_lpa': data.income_lpa,
+        'occupation': data.occupation
+    }])
+
+    prediction = model.predict(input_df)[0]
+
+    return JSONResponse(status_code=200, content={'predicted_category': prediction})
